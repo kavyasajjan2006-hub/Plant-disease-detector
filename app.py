27db25fd3511147f42d7858cd2bae2ef823caec6
kavya <kavyasajjan2006@gmail.com>
2026-04-09 20:24:52 +0530

    Added result display on webpage

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("Hello ")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/submit', methods=['POST'])
+def submit():
+    file=request.files['image']
+    if file:
+        filename=file.filename
+        return f"File uploaded:{filename}"
+    return "No file uploaded"
+
+if __name__ == '__main__':
+    app.run(debug=True)
